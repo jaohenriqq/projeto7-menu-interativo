3ec95e07df975b5b0df819c5b17bdf6b693c391d
jaohenriqq <joaohen.t8@gmail.com>
2024-09-07 17:16:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+do{var escolha = prompt('escolha uma opção: \n 1\n 2\n 3\n 4\n                 5-encerrar')
+   alert(`sua escolha foi ${escolha}`)
+} while(escolha !=5)
+    alert(`programa encerrado`)
+
+
+
+
+
+
+
+  
